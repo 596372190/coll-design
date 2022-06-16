4a49a9740006ffa555b42200aca558d0a05127bf
dragon <596372190@qq.com>
2022-06-16 09:33:47 +0800

    Initial commit

---
+++ src/test/java/com/example/colldesign/CollDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.colldesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/example/colldesign/websocket/config/ServletInitializerTest.java
@@ -0,0 +1,5 @@
+package com.example.colldesign.websocket.config;
+
+class ServletInitializerTest {
+
+}
